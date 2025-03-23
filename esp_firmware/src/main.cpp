ddf3f9624335c56b38a2ff9e2ad54098032cc428
Marvin Bäumer <m.baeumee.industries@gmail.com>
2025-03-23 21:58:13 +0100

    feat: add basic LED control
    The web server responds to POST requests to control the LEDs, turning them on and off. While the server is booting, the chase effect runs on the LED strip.

--- esp_firmware/src/main.cpp
+++ esp_firmware/src/main.cpp
@@ -1,26 +1,49 @@
 #include <Arduino.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266WiFi.h>
+#include <Adafruit_NeoPixel.h>
 
 const char* ssid = ""; // Enter your SSID
 const char* password = ""; // Enter your WiFi password
 
 ESP8266WebServer server(80); // Create an instance of the web server on port 80
 
-void handle_OnConnect() {
-    Serial.println("Connected");   
-}
+#define LED_PIN 2
+#define NUM_LEDS 60
+
+Adafruit_NeoPixel strip(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800); // LED
 
 void handle_ledon() {
     Serial.println("LED ON request received.");
+
+    for (int i = 0; i < NUM_LEDS; i++) {
+        strip.setPixelColor(i, strip.Color(255, 0, 0)); // Red
+    }
+
+    strip.show();
+    server.send(200, "text/plain", "LED's on");
 }
 
 void handle_ledoff() {
     Serial.println("LED OFF request received.");
+    strip.clear();
+    strip.show();
+    
+    server.send(200, "text/plain", "LED's off");
+}
+
+void chaseEffect(uint8_t r, uint8_t g, uint8_t b, int wait) {
+    for (int i = 0; i < NUM_LEDS; i++) {
+        strip.clear();  // Alle LEDs ausschalten
+        strip.setPixelColor(i, strip.Color(r, g, b)); // Aktuelle LED einschalten
+        strip.show();
+        delay(wait);
+    }
 }
 
 void handle_NotFound() {
     Serial.println("404: Not Found");
+    server.send(404, "text/plain", "404 - Page not found");
 }
 
 void setup() {
@@ -31,17 +54,23 @@ void setup() {
     // Connect to the Wi-Fi network
     WiFi.begin(ssid, password);
 
+    // init LED
+    strip.begin();
+
     // Wait until the connection is established
     while (WiFi.status() != WL_CONNECTED) {
         Serial.print(".");
-        delay(1000);  // Wait for 1 second
+        chaseEffect(255, 0, 0, 10);
     }
+
+    strip.clear();
+    strip.show();
+    strip.setBrightness(50);
 
     Serial.println("WiFi connected!");
     Serial.print("IP Address: ");
     Serial.println(WiFi.localIP());  // Print the IP address of the ESP8266
-    
-    server.on("/", handle_OnConnect);      // Handle the root URL
+
     server.on("/ledon", handle_ledon);     // Handle LED on request
     server.on("/ledoff", handle_ledoff);   // Handle LED off request
     server.onNotFound(handle_NotFound);    // Handle all other unknown routes
